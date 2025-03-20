e406b8cc6ad1bccc741bcc2a5b3b6ab8075e4a40
lakshya-ion <lakshya.gupta@iongroup.com>
2025-03-20 15:14:42 +0530

    made some changes to the unit test of bank statement

--- tests/BankStatement.spec.ts
+++ tests/BankStatement.spec.ts
@@ -10,10 +10,10 @@ describe("Statement", () => {
     const consoleSpy = jest.spyOn(console, "log").mockImplementation((msg) => {
       logs.push(msg);
     });
-    //prettier-ignore
-    testAccount.deposit(500.00);
-    //prettier-ignore
-    testAccount.withdraw(200.00);
+    testAccount.deposit(500.0);
+    testAccount.withdraw(200.0);
+    testAccount.withdraw(300.0);
+    testAccount.deposit(100.0);
 
     testStatement.printStatement();
 
@@ -21,6 +21,8 @@ describe("Statement", () => {
       "Date | Amount | Balance",
       "2025-03-20 | +500.00 | 500.00",
       "2025-03-20 | -200.00 | 300.00",
+      "2025-03-20 | -300.00 | 0.00",
+      "2025-03-20 | +100.00 | 100.00",
     ]);
 
     consoleSpy.mockRestore();
